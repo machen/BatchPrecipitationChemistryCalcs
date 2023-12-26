52609aa05526a5ff9136dedf34c9703173f30b9b
machen <machen27@gmail.com>
2023-12-25 23:35:05 -0500

    Convert simulation into individual function

--- BatchPrecip.py
+++ BatchPrecip.py
@@ -21,6 +21,38 @@ def precipitationRateCalculation(ionActivityProduct, equilibriumK, rateConstantk
     return rate
 
 
+def simulatePrecipNoCO3(totCa, totC, k_react, Keq_solid, n_rxnOrder, dt, t_end):
+    # Initialize parameters and calculation arrays
+    times = np.arange(0, t_end+dt, dt)
+    NPoints = np.shape(times)
+    Ca = np.zeros(NPoints)
+    Ca[0] = totCa
+    CO3 = np.zeros(NPoints)
+    CO3[0] = totC
+    rates = np.zeros(NPoints)
+
+    # Begin calculation
+    for timeIndex in range(0, NPoints[0]-1, 1):
+        IAP = Ca[timeIndex]*CO3[timeIndex]
+        rate = precipitationRateCalculation(IAP, Keq_solid, k_react, n_rxnOrder)
+        rates[timeIndex] = rate
+        Ca[timeIndex+1] = Ca[timeIndex]+rate*dt
+        CO3[timeIndex+1] = CO3[timeIndex]+rate*dt
+    rates[-1] = precipitationRateCalculation(Ca[-1]*CO3[-1], Keq_solid, k_react, n_rxnOrder)
+    plt.figure(1)
+    plt.plot(times, Ca, label='Calcium, no equilibrium calc')
+    plt.plot(times, CO3, label='Carbonate, no equilibrium calc')
+    plt.legend()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Concentration (M)')
+
+    plt.figure(2)
+    plt.plot(times, rates)
+    # Save results to dictionary
+    resultDict = {'CO3': CO3, 'Ca': Ca, 'rates': rates, 'times': times}
+    return resultDict
+
+
 def main():
     # User inputs/flags
     Ca_Initial = 1E-3 # Molar
@@ -29,38 +61,9 @@ def main():
     Keq = 10**-8.54
     dt = 1E-3 # s
     t_end = 100
-    k = 1E-6 # Mol/s
-    Keq = 10**-8.54
-    dt = 1E-3 # s
     reactionOrder = 1
 
-    # Initialize parameters and calculation arrays
-    times = np.arange(0, t_end+dt, dt)
-    NPoints = np.shape(times)
-    Ca = np.zeros(NPoints)
-    Ca[0] = Ca_Initial
-    CO3 = np.zeros(NPoints)
-    CO3[0] = CO3_Initial
-    rates = np.zeros(NPoints)
-
-    # Begin calculation
-    for timeIndex in range(0, NPoints[0]-1, 1):
-        IAP = Ca[timeIndex]*CO3[timeIndex]
-        rate = precipitationRateCalculation(IAP, Keq, k, reactionOrder)
-        rates[timeIndex] = rate
-        Ca[timeIndex+1] = Ca[timeIndex]+rate*dt
-        CO3[timeIndex+1] = CO3[timeIndex]+rate*dt
-    rates[-1] = precipitationRateCalculation(Ca[-1]*CO3[-1], Keq, k, reactionOrder)
-    plt.figure(1)
-    plt.plot(times, Ca, label='Calcium')
-    plt.plot(times, CO3, label='Carbonate')
-    plt.legend()
-    plt.xlabel('Time (s)')
-    plt.ylabel('Concentration (M)')
-
-    plt.figure(2)
-    plt.plot(times, rates)
-    plt.show()
+    nonEquilData = simulatePrecipNoCO3(Ca_Initial, CO3_Initial, k, Keq, reactionOrder, dt, t_end)
 
     return 0
 
